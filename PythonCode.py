0f8b8d396695858f84f4db650ae3b3bc72e165d7
James Corbett <VanguardCorbett@gmail.com>
2023-09-19 17:42:37 -0400

    refactor: Better Pycodestyle Conformity.

--- PythonCode.py
+++ PythonCode.py
@@ -1,8 +1,7 @@
-# Python Code Placeholder
-
 def defdef():
-    # Linter Wants Docstring
-    print("I'm a test method in progress.  This probably isn't going to work either.")
+    # Linter Wants Docstring#
+    print("I am  a test method in progress.  This probably isn't " +
+          "going to work either.")
 
 
 defdef()
